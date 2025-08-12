23cabf2f4ff48a7d5c79a6e620371165f154f4e4
eClavel04 <emilio_clavel@hotmail.com>
2025-08-12 13:53:35 -0500

    Creando la variable donde se guardara los nombres

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];  //para crear variable donde se guarden los nombres
+let nombreUsuario = document.getElementById("amigo");
+
+function agregarAmigo() {
+    let nombre = nombreUsuario.value.trim(); // lee el texto y quita espacios extra
+    if (nombre) { // si no está vacío
+        amigos.push(nombre); // lo guarda en el arreglo
+        console.log(amigos); // muestra la lista en la consola
+        nombreUsuario.value = ""; // limpia el campo
+    } else {
+        alert("Escribe un nombre antes de añadir.");
+    }
+}
